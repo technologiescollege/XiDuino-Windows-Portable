183cc731df8924ff960e0b0fdf1217da81803d30
SebCanet <sebastien.canet@ac-nantes.fr>
2014-12-13 18:07:01 +0100

    Nettoyage

--- scratch2_ex.bat
+++ scratch2_ex.bat
@@ -4,4 +4,4 @@ rem fichiers BAT et fork créés par Sébastien CANET
 cls
 SET currentpath=%~dp1
 SET dossier_scratch="Scratch 2.exe"
-start %dossier_scratch% %currentpath%exemples\tout_en_vrac.sb2
+start %dossier_scratch% %currentpath%exemples\joystick.sb2
