28789ff0c0d00432a0b0dd39550b45a9e289011e
SebCanet <sebastien.canet@ac-nantes.fr>
2014-12-13 13:11:45 +0100

    Affichage de l'IP

---
+++ scratch2_ex.bat
@@ -0,0 +1,7 @@
+@echo off
+break ON
+rem fichiers BAT et fork créés par Sébastien CANET
+cls
+SET currentpath=%~dp1
+SET dossier_scratch="Scratch 2.exe"
+start %dossier_scratch% %currentpath%exemples\tout_en_vrac.sb2
