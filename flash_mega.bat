ebbaa46854d94009a86703b4c64ca67fe2ba1b4e
SebCanet <sebastien.canet@ac-nantes.fr>
2015-05-15 22:58:06 +0200

    Update 2.2.1

--- flash_mega.bat
+++ flash_mega.bat
@@ -4,5 +4,5 @@ rem fichiers BAT et fork créés par Sébastien CANET
 SET currentpath=%~dp1
 cd %currentpath%tools
 cls
-.\avrdude -Cavrdude.conf -v -patmega2560 -cwiring -P\\.\COM24 -b115200 -D -V -Uflash:w:s2a-FirmataPlus.Mega.hex:i
+.\avrdude -Cavrdude.conf -v -patmega2560 -cwiring -P\\.\COM4 -b115200 -D -V -Uflash:w:johnny-five_firmata_mega.hex:i
 pause
--- flash_uno.bat
+++ flash_uno.bat
@@ -4,5 +4,5 @@ rem fichiers BAT et fork créés par Sébastien CANET
 SET currentpath=%~dp1
 cd %currentpath%tools
 cls
-.\avrdude -Cavrdude.conf -v -patmega328p -carduino -P\\.\COM13 -b115200 -D -V -Uflash:w:s2a-FirmataPlus.Uno.hex:i
+.\avrdude -Cavrdude.conf -v -patmega328p -carduino -P\\.\COM6 -b115200 -D -V -Uflash:w:johnny-five_firmata_uno.hex:i
 pause
